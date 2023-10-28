fdaa5687144bec5ee8e390437d42588a1315da93
Xiang Shi <xxs166@student.bham.ac.uk>
2023-10-28 16:23:28 +0100

    feat: antlr4 visitor mode

--- klang/main/main.go
+++ klang/main/main.go
@@ -1,7 +1,10 @@
 package main
 
-import _ "github.com/antlr4-go/antlr"
+import (
+	"git.cs.bham.ac.uk/xxs166/uob-project/klang/parserHelper"
+)
 
 func main() {
-
+	p := parserHelper.FromString("1 + 2 + 3")
+	p.Ast()
 }
---
+++ klang/parserHelper/parser.go
@@ -0,0 +1,52 @@
+package parserHelper
+
+import (
+	"git.cs.bham.ac.uk/xxs166/uob-project/klang/ast"
+	"git.cs.bham.ac.uk/xxs166/uob-project/klang/parser"
+	"github.com/antlr4-go/antlr/v4"
+)
+
+// WrappedParser this is self defined parser
+type WrappedParser struct {
+	lexer  *parser.V2Lexer
+	parser *parser.V2Parser
+	errors []string
+}
+
+func (p *WrappedParser) Ast() *ast.Ast {
+	return p.
+		parser.
+		Program().
+		Accept(newVisitor()).(*ast.Ast)
+}
+
+// region Getter & Setter
+
+func (p *WrappedParser) Errors() []string {
+	return p.errors
+}
+
+func (p *WrappedParser) Lexer() *parser.V2Lexer {
+	return p.lexer
+}
+
+func (p *WrappedParser) Parser() *parser.V2Parser {
+	return p.parser
+}
+
+//endregion
+
+// region Constructor
+
+func FromString(s string) *WrappedParser {
+	return FromLexer(parser.NewV2Lexer(antlr.NewInputStream(s)))
+}
+
+func FromLexer(l *parser.V2Lexer) *WrappedParser {
+	return &WrappedParser{
+		lexer:  l,
+		parser: parser.NewV2Parser(antlr.NewCommonTokenStream(l, antlr.TokenDefaultChannel)),
+	}
+}
+
+//endregion
