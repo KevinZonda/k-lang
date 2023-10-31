236865953ca4552d4b8c8288dfdcfcf0815e75d2
Xiang Shi <xxs166@student.bham.ac.uk>
2023-10-31 18:44:43 +0000

    feat: arr assign by index works

--- src/eval/eval.go
+++ src/eval/eval.go
@@ -146,10 +146,37 @@ func (e *Eval) EvalStmt(n node.Stmt) any {
 	panic("not implemented")
 }
 
+func (e *Eval) evalValWithIdx_(idxs []node.Expr, root *any) *any {
+	val := root
+	for _, idxExpr := range idxs {
+		idx := e.EvalExpr(idxExpr)
+		switch idx.(type) {
+		case int:
+			*val = (*val).([]any)[idx.(int)]
+		}
+	}
+	return val
+}
+
 func (e *Eval) EvalAssignStmt(n *node.AssignStmt) any {
 	v := e.EvalExpr(n.Value)
 	// TODO: arr
 	baseV := n.Var.Value[len(n.Var.Value)-1]
+	obj, ok := e.objTable.Get(baseV.Name.Value)
+	if ok && len(baseV.Index) != 0 {
+		switch obj.(type) {
+		case []any:
+			bIdx := baseV.Index[:len(baseV.Index)-1]
+			if tgt := e.evalValWithIdx_(bIdx, &obj); tgt != nil {
+				switch (*tgt).(type) {
+				case []any:
+					((*tgt).([]any))[e.EvalExpr(baseV.Index[len(baseV.Index)-1]).(int)] = v
+				}
+			}
+			e.objTable.Set(baseV.Name.Value, obj)
+			return v
+		}
+	}
 	e.objTable.Set(baseV.Name.Value, v)
 	return v
 }
