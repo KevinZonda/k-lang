0eebd2a1f7763236c377842a406db9c7743c81a4
Xiang Shi <xxs166@student.bham.ac.uk>
2023-11-19 23:29:21 +0000

    test: rename testers to make test better shown in IDEA

---
+++ src/eval/0_unary_expr_test.go
@@ -0,0 +1,26 @@
+package eval_test
+
+import (
+	"git.cs.bham.ac.uk/projects-2023-24/xxs166/src/tester"
+	"testing"
+)
+
+func TestUnaryExpr(t *testing.T) {
+	code := `
+x := 1
+y := -x
+println(y)
+z := -y
+println(z)
+a := !true
+println(a)
+b := !false
+println(b)
+`
+	expected := `-1
+1
+false
+true
+`
+	tester.GeneralTest(false, t, code, expected)
+}
