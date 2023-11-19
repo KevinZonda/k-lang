dfb2dece40e8ea7c1ef8a166f762cbf7f57b0a76
Xiang Shi <xxs166@student.bham.ac.uk>
2023-11-19 22:33:44 +0000

    feat: add test coverage

--- scripts/test.bat
+++ scripts/test.bat
@@ -1,4 +1,8 @@
 @echo off
 cd ./src
-go test ./... -v
+go test -v -coverprofile cover.out ./...
+go tool cover -html cover.out -o cover.html
+del cover.out
+set f=file:///%cd%/cover.html
+start chrome %f%
 cd ..
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd ./src
-go test ./... -v
+go test -v -coverprofile cover.out ./...
+go tool cover -html cover.out -o cover.html
+rm cover.out
+start chrome "file:///$PWD/cover.html"
 cd ..
