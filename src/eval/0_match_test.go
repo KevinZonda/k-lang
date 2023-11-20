d794e2c1cfa55d3c8822cfa1be8ae6648a7b54d5
Xiang Shi <xxs166@student.bham.ac.uk>
2023-11-20 00:10:10 +0000

    test: more test on match

--- src/eval/0_match_test.go
+++ src/eval/0_match_test.go
@@ -1,8 +1,9 @@
 package eval_test
 
 import (
-	"git.cs.bham.ac.uk/projects-2023-24/xxs166/src/tester"
 	"testing"
+
+	"git.cs.bham.ac.uk/projects-2023-24/xxs166/src/tester"
 )
 
 func TestMatchStmt(t *testing.T) {
@@ -21,5 +22,40 @@ match (x) {
 }
 `
 	expected := "eleven\n"
+	tester.GeneralTest(false, t, code, expected)
+}
+
+func TestMatchStmtWithoutDefault(t *testing.T) {
+	code := `
+x := 18
+match (x) {
+    case 7: {
+        println("seven")
+    }
+	case 11: {
+        println("eleven")
+	}
+}
+`
+	expected := ""
+	tester.GeneralTest(false, t, code, expected)
+}
+
+func TestMatchStmtDefault(t *testing.T) {
+	code := `
+x := 19
+match (x) {
+    case 7: {
+        println("seven")
+    }
+	case 11: {
+        println("eleven")
+	}
+	default: {
+        println("default")
+	}
+}
+`
+	expected := "default\n"
 	tester.GeneralTest(false, t, code, expected)
 }
