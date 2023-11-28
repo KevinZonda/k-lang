3bad99b38145a124979a84e373ed00b6c1c59864
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2023-11-28 02:42:50 +0000

    feat(vsc): support LSP pull up

--- vscode-plugin/klang-highlighter/src/extension.ts
+++ vscode-plugin/klang-highlighter/src/extension.ts
@@ -65,13 +65,74 @@ export function activate(context: ExtensionContext) {
     }
 }
 
-function startLsp() {
+let lsp : any;
+let isLspReady = false;
+
+
+function pullUpLspServer(addr: string) {
+    let cmd = klangCfg.get<string>("runner");
+    if (cmd === undefined || cmd === "" || cmd === null) {
+        cmd = "klang"
+    }
+    lspChannel.appendLine("LSP Path: " + cmd)
+    const spawn = require("child_process").spawn;
+    lsp = spawn(
+        cmd, ["lsp", addr],
+        {
+            detatched: true,
+        }
+    );
+
+    lsp.stdout.on("data", (data) => {
+        lspChannel.append(data.toString());
+        lspChannel.appendLine("Lsp ready (STDOUT)");
+        isLspReady = true;
+    });
+
+    lsp.stderr.on("data", (data) => {
+        lspChannel.append(data.toString());
+        lspChannel.appendLine("Lsp ready (STDERR)");
+        isLspReady = true;
+    });
+    lsp.on("close", (code) => {
+        isLspReady = true;
+        runChannel.appendLine("Lsp closed: " + code);
+    });
+}
+
+function strToHostPort(addr: string) : [string, number] {
+    let host = ""
+    let port = 11451;
+    let i = addr.indexOf(":");
+    if (i >= 0) {
+        host = addr.substring(0, i);
+        port = parseInt(addr.substring(i + 1));
+    }
+    return [host, port];
+}
+
+async function startLsp() {
     let lspListenAddr = klangCfg.get<string>("lspListenAddress");
     if (lspListenAddr === undefined || lspListenAddr === "" || lspListenAddr === null) {
         lspListenAddr = "127.0.0.1:11451"
     }
+    pullUpLspServer(lspListenAddr);
     lspChannel.appendLine("LSP Listen Address: " + lspListenAddr);
-    const socket = net.connect(lspListenAddr);
+
+    while (isLspReady === false) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        lspChannel.appendLine("Waiting for LSP to be ready...");
+    }
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    lspChannel.appendLine("LSP is ready");
+    const host = strToHostPort(lspListenAddr);
+    const socket = net.connect(host[1], host[0]);
+    socket.on('connect', () => {
+        lspChannel.appendLine("LSC connected");
+    });
+    socket.on('close', () => {
+        lspChannel.appendLine("LSC closed");
+    });
     const serverOptions = () => {
         return Promise.resolve({
             writer: socket,
@@ -101,5 +162,8 @@ export function deactivate() : Thenable<void> | undefined {
     if (!client) {
 		return undefined;
 	}
+    if (lsp) {
+        lsp.kill();
+    }
     return client.stop();
 }
