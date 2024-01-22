5f189028e988d66f87fed5947e50291e449fb9ee
Xiang Shi <xxs166@student.bham.ac.uk>
2024-01-22 14:31:20 +0000

    test: stack protection test

---
+++ src/eval/0_stackframe_protection_test.go
@@ -0,0 +1,39 @@
+package eval_test
+
+import (
+	"git.cs.bham.ac.uk/projects-2023-24/xxs166/src/tester"
+	"testing"
+)
+
+func TestStackframeProtection(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			str, ok := r.(string)
+			if !ok {
+				t.Errorf("Expected string, got %T", r)
+				return
+			}
+			t.Log(str)
+			if str != "No Var Found: x" {
+				t.Errorf("Expected 'No Var Found: x', got %s", str)
+			}
+
+		} else {
+			t.Errorf("Expected panic, got %v", r)
+		}
+	}()
+	code := `
+fn f() {
+	print(y)
+    x = 18
+    f2()
+}
+fn f2() {
+  print(x)
+}
+y = 11
+f()
+`
+	expected := ""
+	tester.GeneralTest(true, t, code, expected)
+}
