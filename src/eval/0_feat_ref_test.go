a682ba71fbb875a5bb8ec6cccc67e64d38d0bab8
Xiang Shi <xxs166@student.bham.ac.uk>
2024-01-25 13:33:08 +0000

    test: add ref

---
+++ src/eval/0_feat_ref_test.go
@@ -0,0 +1,58 @@
+package eval_test
+
+import (
+	"git.cs.bham.ac.uk/projects-2023-24/xxs166/src/tester"
+	"testing"
+)
+
+func TestRefInt(t *testing.T) {
+	code := `
+fn add1(&x) {
+  	x = x + 1
+}
+x := 12
+println(x)
+add1(x)
+println(x)
+`
+	expected := `12
+13
+`
+	tester.GeneralTest(false, t, code, expected)
+}
+
+func TestRefArr(t *testing.T) {
+	code := `
+fn add1(&x) {
+  	x = x + 1
+}
+x := []
+println(x)
+add1(x)
+println(x)
+`
+	expected := `[]
+[1]
+`
+	tester.GeneralTest(false, t, code, expected)
+}
+
+func TestRefStruct(t *testing.T) {
+	code := `
+struct color {
+  int r, g, b
+  int a = 20
+}
+fn add1(&x) {
+  	x.g = 256
+}
+x := color{}
+println(x)
+add1(x)
+println(x)
+`
+	expected := `color {r: 0, g: 0, b: 0, a: 20}
+color {r: 0, g: 256, b: 0, a: 20}
+`
+	tester.GeneralTest(false, t, code, expected)
+}
