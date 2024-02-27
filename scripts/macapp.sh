76d35a09f62ba544e4eeef88aa92b5f8de7f4d1d
Xiang Shi <xxs166@student.bham.ac.uk>
2024-02-27 01:28:52 +0000

    feat: material

--- scripts/macapp.sh
+++ scripts/macapp.sh
@@ -3,4 +3,5 @@ cd src/out
 rm -fr IDLE.app
 mkdir -p IDLE.app/Contents/MacOS
 cp ../../scripts/macPack/Info.plist IDLE.app/Contents
+cp -r ../../scripts/macPack/Resources IDLE.app/Contents
 cp idle IDLE.app/Contents/MacOS
