d628ea121285439cb1366c7332d0fba4f4eb3003
Xiang Shi <xxs166@student.bham.ac.uk>
2024-04-01 19:33:52 +0100

    fix: and or cannot been fused

--- src/eval/expr_binOper.go
+++ src/eval/expr_binOper.go
@@ -2,11 +2,18 @@ package eval
 
 import (
 	"git.cs.bham.ac.uk/projects-2023-24/xxs166/src/ast/node"
+	"git.cs.bham.ac.uk/projects-2023-24/xxs166/src/ast/token"
 	"git.cs.bham.ac.uk/projects-2023-24/xxs166/src/eval/binaryOperEval"
 )
 
 func (e *Eval) EvalBinOperExpr(n *node.BinaryOperExpr) any {
 	left := e.EvalExpr(n.Left).EnsureValue()
+	if n.Token.Kind == token.Or && left == true {
+		return true
+	}
+	if n.Token.Kind == token.And && left == false {
+		return false
+	}
 	right := e.EvalExpr(n.Right).EnsureValue()
 	left = e.unboxToEnd(left)
 	right = e.unboxToEnd(right)
